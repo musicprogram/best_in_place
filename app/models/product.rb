2d298bd8e39cfd34d765c5b38ce2adc20b75a560
musicprogram <juan-o-henao@hotmail.es>
2017-08-19 08:02:03 -0500

    funcionando validaciones y mensaje de error

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,12 @@
 class Product < ApplicationRecord
+	validates :name, presence: true, uniqueness: true
+
+	validate :validacion_personalized
+
+	def validacion_personalized
+		if self.price < 100
+			errors.add(:price, "Debe ser mayor a 100")
+		end	
+	end
+
 end
